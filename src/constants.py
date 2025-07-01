307a28deca1ca814cd2abc16c2e7c708fb5c0b97
Bielikov Maksym <maximax6767@gmail.com>
2025-07-01 17:50:16 +0300

    feat: add file logger

--- src/constants.py
+++ src/constants.py
@@ -2,3 +2,7 @@ CONFIG_PATH = "./config.json"
 IMAGE_INFOS_CACHE = "./cache.json"
 BASE_URL = "https://konachan.com/post.json"
 SINGLETON_LABEL = "konachan-wallpaper-changer"
+
+LOG_FILE_NAME = "app.log"
+LOG_FILE_MAX_SIZE = 5 * 1024 * 1024
+LOG_FILE_FORMAT_STRING = "%(asctime)s [%(levelname)s] %(message)s"
--- src/logger.py
+++ src/logger.py
@@ -1,9 +1,24 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(message)s",
-    handlers=[logging.StreamHandler()],
+from constants import (
+    LOG_FILE_FORMAT_STRING,
+    LOG_FILE_MAX_SIZE,
+    LOG_FILE_NAME,
 )
+
+formatter = logging.Formatter(LOG_FILE_FORMAT_STRING)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+rotating_handler = RotatingFileHandler(
+    LOG_FILE_NAME,
+    mode="a",
+    maxBytes=LOG_FILE_MAX_SIZE,
+)
+rotating_handler.setFormatter(formatter)
+
+logging.basicConfig(level=logging.INFO, handlers=[stream_handler, rotating_handler])
 
 logger = logging.getLogger("wallpaper_changer")
